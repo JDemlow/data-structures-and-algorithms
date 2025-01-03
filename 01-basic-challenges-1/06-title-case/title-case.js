683dd157dc8665e04f2377be4440b7a05d09908b
JDemlow <jdemlowdev@gmail.com>
2025-01-02 17:55:59 -0700

    mm

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,9 +1,3 @@
-function titleCase(str) {
-  const words = str.toLowerCase().split(" ");
-  for (i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-  }
-  return words.join(" ");
-}
+function titleCase() {}
 
 module.exports = titleCase;
