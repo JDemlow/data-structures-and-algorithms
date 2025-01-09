c85e15f5d6aa0cee9ef1c33d515c900d7af9a7fd
JDemlow <jdemlowdev@gmail.com>
2025-01-09 16:04:02 -0700

    mm

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -4,6 +4,14 @@
 
 // for loop
 
-// function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let maxNum = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > maxNum) {
+      maxNum = arr[i];
+    }
+  }
+  return maxNum;
+}
 
 module.exports = findMaxNumber;
