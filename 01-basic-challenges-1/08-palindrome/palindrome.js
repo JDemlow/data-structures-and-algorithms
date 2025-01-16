05932294763859c61f45c564201b146918196215
JDemlow <jdemlowdev@gmail.com>
2025-01-16 16:31:52 -0700

    mm

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,9 @@
-const isPalindrome = () => {};
+// const isPalindrome = () => { };
+
+function isPalindrome(str) {
+  let formattedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversed = formattedString.split("").reverse().join("");
+  return formattedString === reversed;
+}
 
 module.exports = isPalindrome;
