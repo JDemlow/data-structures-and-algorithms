4a930cb075203de9e08fb9434af57caa606a3110
JDemlow <jdemlowdev@gmail.com>
2025-01-16 16:21:30 -0700

    mm

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,22 +1,3 @@
-function calculator(num1, num2, operator) {
-  let result;
-  switch (operator) {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*":
-      result = num1 * num2;
-      break;
-    case "/":
-      result = num1 / num2;
-      break;
-    default:
-      result = "Invalid Operator";
-  }
-  return result;
-}
+function calculator() {}
 
 module.exports = calculator;
