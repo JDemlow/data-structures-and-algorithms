466ba53eb32dac0d2e2b0797b8d91f8a2c055954
JDemlow <jdemlowdev@gmail.com>
2025-01-27 15:27:38 -0700

    mm

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,14 +1,6 @@
 // Use loop
 
-function countOccurrences(str, char) {
-  let count = 0;
-  for (i = 0; i < str.length; i++) {
-    if (str[i] === char) {
-      count++;
-    }
-  }
-  return count;
-}
+// function countOccurrences() {}
 
 // Use one line solution
 
