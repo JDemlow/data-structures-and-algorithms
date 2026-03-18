6d155a6d7881e37e0162a823331008c0174ead63
JDemlow <jdemlowdev@gmail.com>
2026-03-18 16:40:00 -0600

    case manipulation

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,11 +1,5 @@
-function findMaxNumber(arr) {
-  let max = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
+function findMaxNumber() {
+  // both solutions
 }
 
 module.exports = findMaxNumber;
