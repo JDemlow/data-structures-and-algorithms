0bc614ac316ab45e7f083a468a0fe57194d3bc75
JDemlow <jdemlowdev@gmail.com>
2026-03-19 17:03:37 -0600

    string manipulation

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,7 +1,5 @@
-function isPalindrome(str) {
-  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
-
-  return cleaned === cleaned.split("").reverse().join("");
+function isPalindrome() {
+  // both solutions
 }
 
 module.exports = isPalindrome;
--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,18 @@
-function countVowels() {}
+function countVowels(str) {
+  const formatted = str.toLowerCase();
+  let result = 0;
+
+  for (let i = 0; i < formatted.length; i++) {
+    if (
+      formatted[i] === "a" ||
+      formatted[i] === "e" ||
+      formatted[i] === "i" ||
+      formatted[i] === "o" ||
+      formatted[i] === "u"
+    )
+      result++;
+  }
+  return result;
+}
 
 module.exports = countVowels;
