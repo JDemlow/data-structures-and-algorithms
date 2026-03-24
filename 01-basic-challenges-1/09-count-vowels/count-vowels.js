be4625752b2e380c8be432cca3b0c1e89915c98f
JDemlow <jdemlowdev@gmail.com>
2026-03-23 20:51:42 -0600

    working with Set constructor

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,18 +1,3 @@
-function countVowels(str) {
-  const formatted = str.toLowerCase();
-  let result = 0;
-
-  for (let i = 0; i < formatted.length; i++) {
-    if (
-      formatted[i] === "a" ||
-      formatted[i] === "e" ||
-      formatted[i] === "i" ||
-      formatted[i] === "o" ||
-      formatted[i] === "u"
-    )
-      result++;
-  }
-  return result;
-}
+function countVowels() {}
 
 module.exports = countVowels;
