b99d95b1e0a233dd7dd0e5a4efea30c509d3aec9
JDemlow <jdemlowdev@gmail.com>
2026-04-03 16:44:35 -0600

    regex

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,4 +1,8 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  return cleaned === cleaned.split("").reverse().join("");
+}
 
 module.exports = isPalindrome;
 
