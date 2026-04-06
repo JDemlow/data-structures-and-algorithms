b7dd6882b24e6f346ecdaf8a2e770d271d9eb272
JDemlow <jdemlowdev@gmail.com>
2026-04-06 15:06:14 -0600

    data structures & alogos

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,5 +1,13 @@
-function arrayIntersection() {
-  // both solutions
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const intersection = [];
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersection.push(num);
+    }
+  }
+  return intersection;
 }
 
 module.exports = arrayIntersection;
