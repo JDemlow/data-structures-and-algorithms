d46f33aee33588a4306ab931420e5cf86933b547
JDemlow <jdemlowdev@gmail.com>
2026-04-07 20:55:52 -0600

    display likes

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,13 +1,5 @@
-function arrayIntersection(arr1, arr2) {
-  const set1 = new Set(arr1);
-  const intersection = [];
-
-  for (let num of arr2) {
-    if (set1.has(num)) {
-      intersection.push(num);
-    }
-  }
-  return intersection;
+function arrayIntersection() {
+  // both solutions
 }
 
 module.exports = arrayIntersection;
