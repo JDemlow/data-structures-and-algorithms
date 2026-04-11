063d337a99cdb75f58d6031711be980f2c7fe460
JDemlow <jdemlowdev@gmail.com>
2026-04-10 18:32:18 -0600

    using Set for array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,5 +1,11 @@
-function arrayIntersection() {
-  // both solutions
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  let result = [];
+
+  for (let num of arr2) {
+    if (set1.has(num)) result.push(num);
+  }
+  return result;
 }
 
 module.exports = arrayIntersection;
