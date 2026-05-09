e731466f25a5a80eb64dc3ecd6e1f9e6d03195a0
JDemlow <jdemlowdev@gmail.com>
2026-05-08 20:56:25 -0600

    guass formula

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,7 +1,3 @@
-function isPalindrome(str) {
-  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
-
-  return cleaned === cleaned.split("").reverse().join("");
-}
+function isPalindrome() {}
 
 module.exports = isPalindrome;
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,5 +1,13 @@
-function findMissingNumber() {
-  // both solutions
+function findMissingNumber(arr) {
+  const n = arr.length + 1;
+
+  const expectedTotal = (n * (n + 1)) / 2;
+
+  const actualTotal = arr.reduce((a, b) => a + b, 0);
+
+  return expectedTotal - actualTotal;
 }
 
 module.exports = findMissingNumber;
+
+// both solutions
