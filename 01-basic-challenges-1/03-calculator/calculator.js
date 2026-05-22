29f801ae97e990091d0bda775d85a3eb8c8b6937
JDemlow <jdemlowdev@gmail.com>
2026-05-21 21:01:25 -0600

    jest test

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,23 +1,3 @@
-function calculator(num1, num2, operator) {
-  let result = 0;
-
-  switch (operator) {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*":
-      result = num1 * num2;
-      break;
-    case "/":
-      result = num1 / num2;
-      break;
-    default:
-      return "invalid operator";
-  }
-  return result;
-}
+function calculator() {}
 
 module.exports = calculator;
