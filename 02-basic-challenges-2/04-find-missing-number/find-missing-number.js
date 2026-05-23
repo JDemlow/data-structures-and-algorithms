616690817cce608934c75ec9fa52c5b70d0e221b
JDemlow <jdemlowdev@gmail.com>
2026-05-22 19:08:21 -0600

    gauss formula

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,7 +1,15 @@
-function findMissingNumber() {
-  // both solutions
+function findMissingNumber(arr) {
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    actualSum += arr[i];
+  }
+
+  return expectedSum - actualSum;
 }
 
-module.exports = findMissingNumber;
-
 // both solutions
+
+module.exports = findMissingNumber;
