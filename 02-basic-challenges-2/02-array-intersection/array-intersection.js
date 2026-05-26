498cf31b8270eb17f042680e426a6eaf234b074f
JDemlow <jdemlowdev@gmail.com>
2026-05-26 16:53:05 -0600

    has method on set

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,5 +1,15 @@
-function arrayIntersection() {
-  // both solutions
+function arrayIntersection(arr1, arr2) {
+  let result = [];
+  let set1 = new Set(arr1);
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      result.push(num);
+    }
+  }
+  return result;
 }
 
 module.exports = arrayIntersection;
+
+// both solutions
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,15 +1,5 @@
-function findMissingNumber(arr) {
-  const n = arr.length + 1;
-  const expectedSum = (n * (n + 1)) / 2;
-  let actualSum = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    actualSum += arr[i];
-  }
-
-  return expectedSum - actualSum;
+function findMissingNumber() {
+  // both solutions
 }
-
-// both solutions
 
 module.exports = findMissingNumber;
